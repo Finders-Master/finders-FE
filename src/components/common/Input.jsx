7c81d4a63031f98c693f74e2b27969f4cdab76f6
Ulisessg <ukisessg@gmail.com>
2021-02-26 00:59:39 -0600

    Props input component modified

--- src/components/common/Input.jsx
+++ src/components/common/Input.jsx
@@ -1,15 +1,24 @@
 import React from 'react';
 
-function Input({ inputId, type, label, placeholder }) {
+function Input({
+  inputId,
+  type,
+  label,
+  placeholder,
+  disabled = false,
+  initialValue,
+}) {
   return (
     <>
       <label className="component-input" htmlFor={inputId}>
         <p>{label}</p>
         <input
+          value={initialValue}
+          disabled={disabled}
           required
           name={label}
           placeholder={placeholder}
-          className="component-input__input"
+          className={('component-input__input', disabled && 'disbaled-input')}
           id={inputId}
           type={type}
         />
