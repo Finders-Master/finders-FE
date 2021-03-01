9ad5114bd92e3f63e82ac13fc988734bdefaad27
Ulisessg <ukisessg@gmail.com>
2021-03-01 03:30:54 -0600

    RegExp source added

--- src/utils/regExp.js
+++ src/utils/regExp.js
@@ -1,4 +1,4 @@
-//  Regexp to check password and email format
+//  Regex source: https://www.w3resource.com/javascript/form/javascript-form-validation.php
 const RegExpEmail = /^[a-zA-Z0-9.!#$%&'*+/=?^_`{|}~-]+@[a-zA-Z0-9-]+(?:\.[a-zA-Z0-9-]+)*$/;
 const RegExpPassword = /^(?=.*\d)(?=.*[a-z])(?=.*[A-Z]).{6,30}$/;
 
