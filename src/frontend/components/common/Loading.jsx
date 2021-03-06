053553d9921eed1a49268a22a89476f2b3422c54
Ulisessg <ukisessg@gmail.com>
2021-03-06 01:51:05 -0600

    Lazy loading added

---
+++ src/frontend/components/common/Loading.jsx
@@ -0,0 +1,11 @@
+import React from 'react';
+
+function Loading() {
+  return (
+    <>
+      <h1 style={{ textAlign: 'center', marginTop: '25vh' }}>Loading...</h1>
+    </>
+  );
+}
+
+export default Loading;
--- src/frontend/routers/AppRouter.jsx
+++ src/frontend/routers/AppRouter.jsx
@@ -1,39 +1,48 @@
-import React from 'react';
+import React, { lazy, Suspense } from 'react';
 import {
   Switch,
   Route,
   Redirect,
   BrowserRouter as Router,
 } from 'react-router-dom';
-import AngelInfo from '../components/angel-info/Index';
-import Layout from '../components/Layout/Layout';
-import Home from '../containers/Home/Home';
-import NotFound from '../containers/NotFound/NotFound';
-import Notifications from '../containers/Notifications/Notifications';
-import ProfileAndChangeInfo from '../containers/ProfileAndChangeInfo/ProfileAndChangeInfo';
-import RestartPassword from '../containers/Restart/Restart';
-import Login from '../containers/Login/Login';
-import Register from '../containers/Register/Register';
+
+import LoadingFallback from '../components/common/Loading';
+
+const Home = lazy(() => import('../containers/Home/Home'));
+const Layout = lazy(() => import('../components/Layout/Layout'));
+const AngelInfo = lazy(() => import('../components/angel-info/Index'));
+const NotFound = lazy(() => import('../containers/NotFound/NotFound'));
+const Notifications = lazy(() =>
+  import('../containers/Notifications/Notifications'),
+);
+const ProfileAndChangeInfo = lazy(() =>
+  import('../containers/ProfileAndChangeInfo/ProfileAndChangeInfo'),
+);
+const RestartPassword = lazy(() => import('../containers/Restart/Restart'));
+const Login = lazy(() => import('../containers/Login/Login'));
+const Register = lazy(() => import('../containers/Register/Register'));
 
 const DashBoard = () => (
   <>
-    <Router>
-      <Layout>
-        <div className="router-container">
-          <Switch>
-            <Route exact path="/" component={Home} />
-            <Route exact path="/error-404" component={NotFound} />
-            <Route exact path="/angel" component={AngelInfo} />
-            <Route exact path="/alerta" component={Notifications} />
-            <Route exact path="/profile" component={ProfileAndChangeInfo} />
-            <Route exact path="/restart" component={RestartPassword} />
-            <Route exact path="/login" component={Login} />
-            <Route exact path="/registro" component={Register} />
-            <Redirect to="/error-404" />
-          </Switch>
-        </div>
-      </Layout>
-    </Router>
+    <Suspense fallback={<LoadingFallback />}>
+      <Router>
+        <Layout>
+          <div className="router-container">
+            <Switch>
+              <Route exact path="/" component={Home} />
+              <Route exact path="/error-404" component={NotFound} />
+              <Route exact path="/angel" component={AngelInfo} />
+              <Route exact path="/alerta" component={Notifications} />
+              <Route exact path="/profile" component={ProfileAndChangeInfo} />
+              <Route exact path="/restart" component={RestartPassword} />
+              <Route exact path="/login" component={Login} />
+              <Route exact path="/registro" component={Register} />
+              <Redirect to="/error-404" />
+            </Switch>
+          </div>
+        </Layout>
+      </Router>
+    </Suspense>
   </>
 );
 
