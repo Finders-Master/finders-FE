7b2922f301abc345dfa3205a039a8ce94c72b80f
Ulisessg <ukisessg@gmail.com>
2021-03-08 04:41:31 -0600

    Footer styles fixed

--- src/frontend/components/Layout/Footer.jsx
+++ src/frontend/components/Layout/Footer.jsx
@@ -1,9 +1,42 @@
 import React from 'react';
+import { Link } from 'react-router-dom';
 
 const Footer = () => (
-  <footer style={{padding: '20px 0'}} className="footer">
-    <div></div>
-    <h3 style={{color:'lightgray'}}>FINDME® 2021 ALL RIGHTS RESERVED</h3>
+  <footer className="footer">
+    <ul className="footer--list">
+      <li>
+        <Link to="/">
+          <img
+            src="https://firebasestorage.googleapis.com/v0/b/web-projects-50e7e.appspot.com/o/finders%2FHome%20icon.png?alt=media&token=c40167f6-7e82-43c9-8a3d-41f688eb4c36"
+            alt="Home icon"
+          />
+        </Link>
+      </li>
+      <li>
+        <Link to="/scan">
+          <img
+            src="https://firebasestorage.googleapis.com/v0/b/web-projects-50e7e.appspot.com/o/finders%2FSearch%20icon.png?alt=media&token=1a88c2f3-e8c2-49a8-b09c-bc097ae6c284"
+            alt="Escanear código QR"
+          />
+        </Link>
+      </li>
+      <li>
+        <Link to="/notificaciones">
+          <img
+            src="https://firebasestorage.googleapis.com/v0/b/web-projects-50e7e.appspot.com/o/finders%2FNotification%20icon.png?alt=media&token=44ded482-d518-4611-b4a3-1d296ad8d84a"
+            alt="Notificaciones"
+          />
+        </Link>
+      </li>
+      <li>
+        <Link to="perfil">
+          <img
+            src="https://firebasestorage.googleapis.com/v0/b/web-projects-50e7e.appspot.com/o/finders%2FAccount%20icon.png?alt=media&token=ce4cf284-e57d-4ef5-ba8e-d0070b5d4c1b"
+            alt="Perfil"
+          />
+        </Link>
+      </li>
+    </ul>
   </footer>
 );
 export default Footer;
--- src/frontend/containers/Home/Home.jsx
+++ src/frontend/containers/Home/Home.jsx
@@ -9,21 +9,20 @@ import IconLocation from '../../assets/static/icon-location.png';
 import IconHearth from '../../assets/static/hearth.png';
 
 const Home = () => (
-
   <>
     <Banner />
     <section className="Home">
       <article className="Home__description">
-        <img src={Iphone} alt="telefono movil con la pantalla escaneando un codigo qr FindMe" 
-        className="animate__animated animate__fadeInUp animate__delay-1s"
+        <img
+          src={Iphone}
+          alt="telefono movil con la pantalla escaneando un codigo qr FindMe"
+          className="animate__animated animate__fadeInUp animate__delay-1s"
         />
         <div className="Home__description__text">
           <h2>
             <span>FINDME, </span>
-            es una aplicación diseñada
-            para atender una urgencia en
-            personas con enfermedades degenerativas
-            como aizhaimer entre otras enfermedades
+            es una aplicación diseñada para atender una urgencia en personas con
+            enfermedades degenerativas como aizhaimer entre otras enfermedades
           </h2>
           <img
             src={GivingCard}
@@ -36,33 +35,43 @@ const Home = () => (
         <div className="Home__instrucctions__container">
           <div>
             <h3>¿COMO FUNCIONA?</h3>
-            <h4>Si encuentras a alguien con complicaciones seguramente esta inscrito en FINDME</h4>
+            <h4>
+              Si encuentras a alguien con complicaciones seguramente esta
+              inscrito en FINDME
+            </h4>
           </div>
           <div className="Home__instrucctions__container__icons">
             <div>
-              <img src={ScaningIstrucction} alt="instrucciones icono de pantalla escaneando un qr" />
+              <img
+                src={ScaningIstrucction}
+                alt="instrucciones icono de pantalla escaneando un qr"
+              />
               <p>Localiza la tarjeta con el codigo QR</p>
             </div>
             <div>
-              <img src={HandMobile} alt="instrucciones icono de pantalla escaneando un qr" />
+              <img
+                src={HandMobile}
+                alt="instrucciones icono de pantalla escaneando un qr"
+              />
               <p>Escanealo con la camara de tu celular o aquí</p>
             </div>
             <div>
-              <img src={IconCard} alt="instrucciones icono de pantalla escaneando un qr" />
+              <img
+                src={IconCard}
+                alt="instrucciones icono de pantalla escaneando un qr"
+              />
               <p>
-                serás enviado a una página
-                con los datos personales de
-                la persona asi como indicacones
-                para su auxilio
+                serás enviado a una página con los datos personales de la
+                persona asi como indicacones para su auxilio
               </p>
             </div>
             <div>
-              <img src={IconLocation} alt="instrucciones icono de pantalla escaneando un qr" />
+              <img
+                src={IconLocation}
+                alt="instrucciones icono de pantalla escaneando un qr"
+              />
               <p>
-                La aplicación
-                mandara la ubicación
-                de la persona a su
-                guardian
+                La aplicación mandara la ubicación de la persona a su guardian
               </p>
             </div>
           </div>
@@ -70,19 +79,21 @@ const Home = () => (
       </article>
       <article className="Home__helpus">
         <div className="Home__helpus__container">
-          <img src={IconHearth} alt="icono de un corazon pixeleado, parte del logo de findme"
+          <img
+            src={IconHearth}
+            alt="icono de un corazon pixeleado, parte del logo de findme"
             className="animate__animated animate__heartBeat animate__infinite"
           />
           <h2>AYUDANOS A SALVAR VIDAS</h2>
           <h4>
-            RESGISTRATE COMO GUARDIAN Y
-            DA DE ALTA A UN CONOCIDO O FAMILIAR TUYO,
-            UN DÍA PUEDE NECESITAR TU AYUDA Y
-            SABRAS DONDE ENCONTRARLO
+            RESGISTRATE COMO GUARDIAN Y DA DE ALTA A UN CONOCIDO O FAMILIAR
+            TUYO, UN DÍA PUEDE NECESITAR TU AYUDA Y SABRAS DONDE ENCONTRARLO
           </h4>
         </div>
       </article>
     </section>
+
+    <h3 style={{ color: 'lightgray' }}>FINDME® 2021 ALL RIGHTS RESERVED</h3>
   </>
 );
 export default Home;
