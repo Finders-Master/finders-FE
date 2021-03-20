fed5727a39996ef92be687b1ae45e46eb7fc2ec2
ulisessg <ukisessg@gmail.com>
2021-03-19 23:25:58 -0600

    Camera access finished

--- src/frontend/components/Scan/Camera.jsx
+++ src/frontend/components/Scan/Camera.jsx
@@ -1,9 +1,8 @@
-import React, { useLayoutEffect, useRef, useState } from 'react';
-import Sweet from 'sweetalert2';
+import React, { useLayoutEffect, useState } from "react";
+import Sweet from "sweetalert2";
 
 function Camera() {
-  const VideoElement = useRef(null);
-
+  const [photoTaked, setPhotoTaked] = useState(false);
   // Solicite camera permissions
   function useCamera() {
     if (
@@ -15,58 +14,116 @@ function Camera() {
       navigator.getUserMedia(
         { video: true },
         () => {
-          const stream = new MediaStream();
+          let streaming = false;
+          const video = document.querySelector("#video");
+          const canvas = document.querySelector("#canva");
+          const photo = document.querySelector("#photo");
+          const startbutton = document.querySelector("#takePhoto");
+          const width = 320;
+          let height = 0;
 
-          if (navigator.mozGetUserMedia) {
-            VideoElement.current.mozSrcObject = stream;
-          } else {
-            VideoElement.current.srcObject = stream;
+          navigator.getMedia =
+            navigator.getUserMedia ||
+            navigator.webkitGetUserMedia ||
+            navigator.mozGetUserMedia ||
+            navigator.msGetUserMedia;
+
+          navigator.getMedia(
+            {
+              video: true,
+              audio: false,
+            },
+            (stream) => {
+              if (navigator.mozGetUserMedia) {
+                video.mozSrcObject = stream;
+              } else {
+                video.srcObject = stream;
+              }
+              video.play();
+            },
+            (err) => {
+              Sweet.fire({
+                icon: "warning",
+                text: err,
+              });
+            }
+          );
+
+          video.addEventListener(
+            "canplay",
+            () => {
+              if (!streaming) {
+                height = video.videoHeight / (video.videoWidth / width);
+                video.setAttribute("width", width);
+                video.setAttribute("height", height);
+                canvas.setAttribute("width", width);
+                canvas.setAttribute("height", height);
+                streaming = true;
+              }
+            },
+            false
+          );
+
+          function takepicture() {
+            canvas.width = width;
+            canvas.height = height;
+            canvas.getContext("2d").drawImage(video, 0, 0, width, height);
+            const data = canvas.toDataURL("image/png");
+            photo.setAttribute("src", data);
+            setPhotoTaked(true);
           }
-          VideoElement.current.play();
+
+          startbutton.addEventListener(
+            "click",
+            (ev) => {
+              ev.preventDefault();
+              takepicture();
+            },
+            false
+          );
         },
         () => {
           // Error getting permisions
           Sweet.fire({
-            title: 'Ocurrió un error',
-            icon: 'error',
-            text: 'Si no has habilitado los permisos puedes cambiar esto',
+            title: "Ocurrió un error",
+            icon: "error",
             confirmButtonText:
               '<a style="color: white;" target="blank" href="https://support.google.com/chrome/answer/2693767?co=GENIE.Platform%3DDesktop&hl=es-419">¿Cómo cambiar los permisos?</a>',
             allowOutsideClick: false,
-            html: '<a href="/">Ir al inicio</a>',
+            html: `<p>Si no has habilitado los permisos es necesatio para usar la cámara</p>
+            <a href="/">Regresar al inicio</a>`,
           });
-        },
+        }
       );
     }
   }
 
   useLayoutEffect(() => {
     Sweet.fire({
-      title: 'Permisos requeridos',
+      title: "Permisos requeridos",
       text:
-        'Necesitamos tu permiso para acceder a la cámara para escanear el códigoQR',
-      icon: 'info',
-      confirmButtonText: 'Entendido',
+        "Necesitamos tu permiso para acceder a la cámara para escanear el códigoQR",
+      icon: "info",
+      confirmButtonText: "Entendido",
     }).finally(() => {
       useCamera();
     });
-  }, [VideoElement]);
+  }, []);
 
   return (
     <>
-      {/* <canvas
-        width="300px"
-        height="300px"
-        className="scan-page__container--camera"
-      /> */}
-      <video ref={VideoElement} src="" />
+      {/* eslint-disable-next-line jsx-a11y/media-has-caption */}
+      <video id="video" />
 
       <button
         className="button loginform__submit button-change-info"
         type="button"
+        id="takePhoto"
       >
-        Escanear QR
+        {photoTaked ? "Tomar la foto de nuevo" : "Escanear QR"}
       </button>
+      <canvas id="canva" />
+      <img hidden id="photo" src="" alt="" />
     </>
   );
 }
