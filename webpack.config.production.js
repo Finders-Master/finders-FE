1d25741da8ac21d2f3e9c930ccbcbf73e3535bd2
ulisessg <ukisessg@gmail.com>
2021-03-24 22:26:16 -0600

    Webpack build added

--- webpack.config.production.js
+++ webpack.config.production.js
@@ -3,13 +3,15 @@ const HtmlWebpackPlugin = require('html-webpack-plugin');
 const TerserPlugin = require('terser-webpack-plugin');
 const CompressionPlugin = require('compression-webpack-plugin');
 const MiniCssExtractPlugin = require('mini-css-extract-plugin');
+const AddAssetHtml = require('add-asset-html-webpack-plugin');
 const { DllReferencePlugin } = require('webpack');
+require('dotenv').config();
 
 module.exports = {
   mode: 'production',
-  entry: join(__dirname, 'src', 'index.jsx'),
+  entry: join(__dirname, 'src', 'frontend', 'index.jsx'),
   output: {
-    path: join(__dirname, 'dist'),
+    path: join(__dirname, 'src', 'server', 'public'),
     filename: 'js/[name].[fullhash].js',
   },
   target: ['web', 'es5'],
@@ -51,8 +53,21 @@ module.exports = {
         },
       },
       {
-        test: /\.s[ac]ss$/i,
-        use: [MiniCssExtractPlugin.loader, 'css-loader', 'sass-loader'],
+        test: /\.(s*)css$/,
+        use: [
+          { loader: MiniCssExtractPlugin.loader },
+          'css-loader',
+          'sass-loader',
+        ],
+      },
+      {
+        test: /\.(png|jpe?g|gif)$/i,
+        use: [
+          {
+            loader: 'file-loader',
+            options: { name: 'assets/[name].[ext]' },
+          },
+        ],
       },
     ],
   },
@@ -64,11 +79,23 @@ module.exports = {
     }),
     new HtmlWebpackPlugin({
       template: join(__dirname, 'public', 'index.html'),
-      favicon: join(__dirname, 'src', 'assets', 'finders-logo.png'),
     }),
     new DllReferencePlugin({
       context: __dirname,
-      manifest: join(__dirname, 'dist', 'js', 'modules-manifest.json'),
+      manifest: join(
+        __dirname,
+        'src',
+        'server',
+        'public',
+        'dll',
+        'modules-manifest.json'
+      ),
+    }),
+    new AddAssetHtml({
+      filepath: join(__dirname, 'src', 'server', 'public', 'dll'),
+      files: [
+        join(__dirname, 'src', 'server', 'public', 'dll', 'modules.dll.js'),
+      ],
     }),
   ],
 };
